5277bbf71a3969b8a02ec4ac34c3ec86d6c25316
Franco <freconur@gmail.com>
2024-11-30 01:42:04 -0500

    agredando nueva seccion de la ladingpage

--- pages/landing/index.tsx
+++ pages/landing/index.tsx
@@ -126,16 +126,72 @@ const LandingPageAttendance = () => {
         </div>
 
       </div>
-      <section className="bg-gradient-to-r from-cyan-500 to-blue-500 pb-10">
-        <h3 className='text-2xl uppercase text-sidebarHover text-center font-bold font-montserrat py-[50px]'>beneficios de usar nuestros productos</h3>
-        <ul className='text-white text-xl grid gap-5 justify-center p-5'>
-          <li>1- Mejora la comunicación académica de tus estudiantes con los padres de familia.</li>
-          <li>2- Envia notificaciones a los padres de familia de manera inmediata.</li>
-          <li>3- Gestiona las asistencias de los estudiantes con mensajeria en whatsapp en tiempo real.</li>
-          <li>4- Gestiona el cuaderno de control de manera digital.</li>
-          <li>5- Gestiona y notifica las tareas de clases.</li>
-          <li>6- Dispondras de un chatbot para las consultas academicas de los estudiantes.</li>
+      <section className="bg-gradient-to-r from-cyan-400 to-blue-400 pb-10">
+        <h3 className='text-2xl uppercase text-blue-950 text-center font-bold font-montserrat py-[50px]'>beneficios de usar nuestros productos</h3>
+        <ul className=' text-xl grid gap-5 text-blue-950 justify-center p-5'>
+          <li className='flex gap-3'>
+            <div>
+              1-
+            </div>
+            <div>
+              Mejora la comunicación académica de tus estudiantes con los padres de familia.
+            </div>
+          </li>
+          <li className='flex gap-3'>
+            <div>
+              2-
+            </div>
+            <div>
+              Envia notificaciones a los padres de familia de manera inmediata.
+            </div>
+          </li>
+
+          <li className='flex gap-3'>
+            <div>
+              3-
+            </div>
+            <div>
+              Gestiona las asistencias de los estudiantes con mensajeria en whatsapp en tiempo real.
+            </div>
+          </li>
+
+          <li className='flex gap-3'>
+            <div>
+              4-
+            </div>
+            <div>
+              Gestiona el cuaderno de control de manera digital.
+            </div>
+          </li>
+
+          <li className='flex gap-3'>
+            <div>
+              5-
+            </div>
+            <div>
+              Gestiona y notifica las tareas de clases.
+            </div>
+          </li>
+
+          <li className='flex gap-3'>
+            <div>
+              6-
+            </div>
+            <div>
+              Dispondras de un chatbot para las consultas academicas de los estudiantes.
+            </div>
+          </li>
+
         </ul>
+      </section>
+
+      <section>
+        <div>
+          <h3>Quieres mejorar la comunicacion entre tu institución educativa, padres de familia y estudiantes?</h3>
+          <div>Muchas veces una comunicacion efectiva en un entorno educativo no es facil de lograr, a veces puedes tonarse lento en hacer llegar comunicados a los padres de familia, brindar información académica de los estudiantes a los papas y casos como estos hay muchos, asi que en mirasoft hemos pensados en una manera de poder facilitar y acortar el tiempo de comunicación en ambientes academicos.
+            hemos desarrollado un software diseñado para colegios de nivel primaria y secundaria para facilitar la gestion academica, con la ayuda de nuestra aplicacion lograrás faciliar informacion academica a los padres de familia y estudiantes de tu institución.
+          </div>
+        </div>
       </section>
     </div>
   )
