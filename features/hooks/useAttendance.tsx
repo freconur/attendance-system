d12f8f77f6c2201492337b7631b89d56794fa51a
Franco <freconur@gmail.com>
2025-03-19 00:48:40 -0500

    agregando negrita a la palabra de tardanza

--- features/hooks/useAttendance.tsx
+++ features/hooks/useAttendance.tsx
@@ -130,7 +130,7 @@ export const useAttendance = () => {
             .post(`${URL_API}/v1/messages`,
               {
                 number: `51${findStudent.firstNumberContact}`,
-                message: `Sr.(a) ${findStudent.firstContact}, el estudiante ${findStudent.name} ${findStudent.lastname}, ${rta() ? 'se retiro del colegio a las' : 'acaba de ingresar al colegio a las'} ${dateConvertObjectStudent(currentlyHour)}, ${validacionPuntualTardanza(currentlyHour) === false && 'que es considerado como tardanza'}`
+                message: `Sr.(a) ${findStudent.firstContact}, el estudiante ${findStudent.name} ${findStudent.lastname}, ${rta() ? 'se retiro del colegio a las' : 'acaba de ingresar al colegio a las'} ${dateConvertObjectStudent(currentlyHour)}, ${validacionPuntualTardanza(currentlyHour) === false && 'que es considerado como *tardanza*'}`
               })
             .then(r => {
               if (findStudent.secondContact && findStudent?.secondNumberContact?.length === 9) {
